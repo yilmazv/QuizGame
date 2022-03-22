372ef372bb98e844a6f9a6ee12b150437476e68a
Vehbi Yilmaz <yilmazvtc@gmail.com>
2022-03-21 21:30:20 -0700

    starting code for coding quiz game

---
+++ script.js
@@ -0,0 +1,9 @@
+let startQuiz = document.getElementsByClassName("startbutton");
+
+startQuiz.addEventListener("click", function () {
+  gameStart();
+});
+
+function gameStart() {
+  console.log("starts");
+}
