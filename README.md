6c3b7ec7a2e6a00dce96c60d83254ed0aa02829f
Vehbi Yilmaz <yilmazvtc@gmail.com>
2022-03-23 21:46:18 -0700

    finished coding within working website

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# QuizGame
+Creating a coding quiz. For just this test care i only created two questions and
+was able to interate through the questions and answer list. I was able to subtract time when a question is marked wrong and then also i was able to have and end game function that records the user name and saves it local data.
+
+SS : ![Image of website](ss.png)
+
+Link to deployed application: https://yilmazv.github.io/QuizGame/
--- script.js
+++ script.js
@@ -3,34 +3,61 @@ let showQuestions = document.getElementById("answers");
 let timerShow = document.getElementById("time");
 let questionEl = document.getElementById("questions");
 let answersEl = document.getElementById("answers");
-let mixQuestions;
-let indexCurrent;
-let questions = [
-  {
-    question: "Just testing the questions",
-    answer: [
-      { text: "1", correct: true },
-      { text: "2", correct: false },
-      { text: "3", correct: false },
-      { text: "4", correct: false },
-    ],
-  },
+let playerNameEl = document.getElementById("user");
+let count = 0;
+let holdingIndex = 0;
+let sec = 60;
+
+// let questions = [
+//   {
+//     question: "Just testing the questions",
+//     answer: [
+//       { text: "1", correct: true },
+//       { text: "2", correct: false },
+//       { text: "3", correct: false },
+//       { text: "4", correct: false },
+//     ],
+//   },
+// {
+//     question: "Just testing the questions",
+//     answer: [
+//       { text: "1", correct: true },
+//       { text: "2", correct: false },
+//       { text: "3", correct: false },
+//       { text: "4", correct: false },
+//     ],
+//   },
+// ];
+// creating the questions and answers object and array.
+let questions = ["Select the lowest number", "Selecte the highest number"];
+let answers = [
+  [
+    { text: "1", correct: true },
+    { text: "2", correct: false },
+    { text: "3", correct: false },
+    { text: "4", correct: false },
+  ],
+  [
+    { text: "1", correct: true },
+    { text: "2", correct: false },
+    { text: "3", correct: false },
+    { text: "4", correct: false },
+  ],
 ];
+
+// console.log(answers[holdingIndex].length);
 
 startQuiz.addEventListener("click", function () {
   gameStart();
   timer();
-  nextQuestion();
+  displayQuestion();
 });
 
 function gameStart() {
   startQuiz.classList.add("hide");
-  mixQuestions = questions.sort(() => Math.random() - 0.5);
-  indexCurrent = 0;
   showQuestions.classList.remove("hide");
 }
 function timer() {
-  let sec = 60;
   let timer = setInterval(function () {
     timerShow.textContent = "Time Left: " + sec;
     sec--;
@@ -38,23 +65,60 @@ function timer() {
       clearInterval(timer);
     }
   }, 1000);
+  return sec;
 }
+// this is the function that that goes to the array and selects the question and then selects and answer
+function displayQuestion() {
+  questionEl.innerText = questions[holdingIndex];
+  for (let i = 0; i < answers[holdingIndex].length; i++) {
+    let answerBtns = document.getElementById(`${i}`);
+    answerBtns.innerText = answers[holdingIndex][i].text;
+    answersEl.appendChild(answerBtns);
+    answerBtns.addEventListener("click", () => {
+      holdingIndex++;
+      if (holdingIndex < questions.length) {
+        displayQuestion();
+        // console.log(answers);
+        // console.log(answers[i][i]).correct;
 
-function nextQuestion() {
-  displayQuestion(mixQuestions[indexCurrent]);
-}
-
-
-function displayQuestion(question) {
-  questionEl.innerText = question.question;
-  question.answer.array.forEach((answer) => {
-    let answerBtns = document.createElement("button");
-    answerBtns.innerText = answer.text;
-    answerBtns.classList.add("btn");
-    if(answer.correct = true){
-      answerBtns.dataset.correct = answer.correct
-    }
+        if (answers[i][i].correct === true) {
+          count++;
+          console.log(count);
+          displayQuestion();
+        }
+      } else {
+        sec = sec - 1;
+      }
+      if (i > questions.length || sec == 0) {
+        gameEnd();
+      }
+    });
   }
-};
-function correctAnswer(){}
-function gameEnd() {}
+}
+// promts the user to put their initals and stores it to local storage
+function gameEnd() {
+  let nameOfPlayer = prompt("Enter your initals");
+  localStorage.setItem("nameofplayer", nameOfPlayer);
+  localStorage.setItem("score", JSON.stringify(count));
+  playerNameEl.innerText = nameOfPlayer + " , " + count;
+  sec = 0;
+}
+// add class to buttons
+// hide old buttons
+// add a check inside of event listener
+// if answers.correct = true
+// max number check for the question
+// }
+// //   questionEl.innerText = question.question;
+// //   question.answer.forEach((answer) => {
+// //     let answerBtns = document.createElement("button");
+// //     answerBtns.innerText = answer.text;
+// //     answerBtns.classList.add("btn");
+// //     if ((answer.correct = true)) {
+// //       answerBtns.dataset.correct = answer.correct;
+// //     }
+// //     answerBtns.addEventListener("click",correctAnswer() => {
+// //       answersEl.appendChild(answerBtns)
+// //     })
+// //   });
+// // }
